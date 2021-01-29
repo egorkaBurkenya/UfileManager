4a249ef48e47f75cc3460a6a38860442a8f85bea
egorkaBurkenya <sleepyegorka@gmail.com>
2021-01-29 12:25:01 +0300

    some commit

--- uMen.js
+++ uMen.js
@@ -1,6 +1,19 @@
 'use strict';
 
 const spawn = require('child_process').spawn;
+const fs = require('fs');
 
-const update = spawn('git', ['pull'])
-update.stdout.pipe(process.stdout);
+fs.readFile('info.json', (err, data) => {
+  spawn('git', ['pull']).stdout.pipe(process.stdout);
+  if (err) throw err;
+  let info = JSON.parse(data);
+  console.log(info);
+});
+
+
+// console.group('uMen')
+// console.log(`Version - ${info.version}`)
+// console.groupEnd();
+
+
+
